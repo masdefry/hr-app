8aba4c1da0c58077250f72bfadf33bbd04fea2d8
masdefry <muhammaddefryan@gmail.com>
2024-11-05 15:56:56 +0700

    fix:code

--- src/components/HeaderTitle.tsx
+++ src/components/HeaderTitle.tsx
@@ -1,4 +1,4 @@
-export default function HeaderTitle({title}){
+export default function HeaderTitle({title}: {title: string}){
     return(
         <section className='bg-red-500 text-white flex justify-center p-5 rounded-b-md'>
             <h1 className='text-xl font-bold'>
